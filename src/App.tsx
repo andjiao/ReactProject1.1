b3ba3c8dfd9690380df8f548ba107154afb0cd6d
andreajiao <andreajiao@gmail.com>
2023-08-18 14:11:00 +0200

    first push

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
+import GameGrid from './components/GameGrid';
+
+function App() {
+
+  return <Grid templateAreas={{
+    base: `"nav" "main"`,
+    lg: `"nav nav" "aside main"` // for screen that is wider than 1024px
+  }}>
+
+    <GridItem area="nav">
+      <NavBar/>
+    </GridItem>
+    <Show above="lg">
+    <GridItem area="aside">Aside</GridItem>
+    </Show>
+    <GridItem area="main">
+      <GameGrid/>
+      </GridItem>
+  </Grid>
+  
+}
+
+export default App
