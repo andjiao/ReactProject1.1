dfaa717ec4158cd351a8a2a633e6cc09d7fdaed2
andreajiao <andreajiao@gmail.com>
2023-08-22 10:23:33 +0200

    displaying plaform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
-import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
+import PlatformIconList from './PlatformIconList'
 
 interface Props {
     game: Game
@@ -12,7 +12,8 @@ const GameCard = ({game}: Props) => {
         <Image src={game.background_image}/>
         <CardBody>
             <Heading fontSize="2xl">{game.name}</Heading>
-            {game.parent_platforms.map(({platform}) => <Text>{platform.name}</Text>)}
+            <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
+            
         </CardBody>
     </Card>
   )
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,4 +1,18 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
 
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { IconType } from "react-icons";
+
+import { Icon, HStack } from '@chakra-ui/react';
 import { Platform } from '../hooks/useGames';
 interface Props {
     platforms: Platform[]
@@ -6,8 +20,38 @@ interface Props {
 }
 
 const PlatformIconList = ({platforms}: Props) => {
+    /* the [key: string] is an index signature, 
+    this represents a key property in this object.
+    so here we hae various keys like pc, playstation etc. 
+    by using an indec signature syntax, we do not have to specify 
+    the name of the various keys.
+    so heere we say, that the object has any number of keys of type string.
+    */
+    const iconMap: {[key: string]: IconType}= { 
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux, 
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe
+  }
+
   return (
-    <div>PlatformIconList</div>
+      /* When setting the margin to a plain number, not in a string, 
+      the number will be multiple of the theme 
+      the default value in chakra is 4. so if the marginU={1},
+      the actual number will be 4
+      when putting the number in a string, we get exactly the number we write in the string
+      */
+      <HStack marginY="10px"> 
+     
+    {platforms.map(platform => 
+        <Icon as = {iconMap[platform.slug]} color='gray.500'/>
+    )}
+     </HStack>
   )
 }
 
