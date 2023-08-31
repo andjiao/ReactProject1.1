ca150c28edf432bcd0fd01b5fe945be0ec71d096
andreajiao <andreajiao@gmail.com>
2023-08-31 09:06:15 +0200

    sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,11 @@ const useGames =(
 useData<Game>("/games", {
     params: {
         genres: gameQuery.genre?.id, 
-        platform: gameQuery.platform?.id}},
+        platform: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder
+    
+    }},
+
 [gameQuery] )
 
 export default useGames;
