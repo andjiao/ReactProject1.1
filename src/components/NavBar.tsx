e9914304b71f25cc4b0da88834ae6f9f61c12f3d
andreajiao <andreajiao@gmail.com>
2023-09-01 09:05:52 +0200

    implementing seearching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,15 @@ import logo from "../assets/logo.webp"
 import ColorModeSwitch from './ColorModeSwitch';
 import Searchinput from './Searchinput';
 
+interface Props {
+    onSearch: (searchText: string)=> void
+}
 
-const NavBar = () => {
+const NavBar = ({onSearch}:Props) => {
   return (
     <HStack padding="10px">
         <Image src={logo} boxSize="60px"></Image>
-        <Searchinput/>
+        <Searchinput onSearch={onSearch}/>
         <ColorModeSwitch/>
     </HStack>
   )
